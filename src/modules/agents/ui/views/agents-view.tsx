2db66c9e2beae22981a936cedf638cee60fda588
SumitKadam451 <sumitkadam451@gmail.com>
2025-11-04 16:37:02 +0530

    ResponsiveDialog

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -5,11 +5,15 @@ import { useTRPC } from "@/trpc/client";
 import { LoadingState } from "@/components/loading-state";
 import { ErrorState } from "@/components/error-state";
 
+
 export const AgentsView = () => {
   const trpc = useTRPC();
   const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
 
-  return <div>{JSON.stringify(data, null, 2)}</div>;
+  return <div>
+    
+    {JSON.stringify(data, null, 2)}
+  </div>;
 };
 
 export const AgentsViewLoading = () => {
